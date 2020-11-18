2a941f5341945756b66eb6df9e3a8b37e6535db9
diofinance <diofinance@protonmail.com>
2020-11-18 23:05:01 +0100

    YGY LGE deposits

--- contracts/RAMv1Router.sol
+++ contracts/RAMv1Router.sol
@@ -12,11 +12,13 @@ import "./IRAMVault.sol";
 // This contract is supposed to streamline liquidity additions
 // By allowing people to put in any amount of ETH or YGY and get LP tokens back
 contract RAMv1Router is OwnableUpgradeSafe, VRFConsumerBase {
-    mapping(address => uint256) public hardRAM;
+    mapping(address => uint256) public hardRAMInYGY;
 
     // RAM protocol variables
     address public _RAMToken;
-    address public _RAMWETHPair;
+    address public _YGYRAMPair;
+    address public _YGYToken;
+    address public _YGYWETHPair;
     IFeeApprover public _feeApprover;
     IRAMVault public _RAMVault;
     IWETH public _WETH;
@@ -31,7 +33,7 @@ contract RAMv1Router is OwnableUpgradeSafe, VRFConsumerBase {
     bytes32 internal keyHash;
     uint256 internal fee;
 
-    constructor(address RAMToken, address WETH, address uniV2Factory, address RAMWethPair, address feeApprover, address RAMVault)
+    constructor(address RAMToken, address YGYToken, address WETH, address uniV2Factory, address YGYRAMPair, address YGYWethPair, address feeApprover, address RAMVault)
         VRFConsumerBase(
             0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator (KOVAN)
             0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token (KOVAN)
@@ -39,10 +41,12 @@ contract RAMv1Router is OwnableUpgradeSafe, VRFConsumerBase {
         public
     {
         _RAMToken = RAMToken;
+        _YGYToken = YGYToken;
         _WETH = IWETH(WETH);
         _uniV2Factory = uniV2Factory;
         _feeApprover = IFeeApprover(feeApprover);
-        _RAMWETHPair = RAMWethPair;
+        _YGYRAMPair = YGYRAMPair;
+        _YGYWETHPair = YGYWethPair;
         _RAMVault = IRAMVault(RAMVault);
         refreshApproval();
 
@@ -52,7 +56,7 @@ contract RAMv1Router is OwnableUpgradeSafe, VRFConsumerBase {
 
 
     function refreshApproval() public {
-        IUniswapV2Pair(_RAMWETHPair).approve(address(_RAMVault), uint(-1));
+        IUniswapV2Pair(_YGYRAMPair).approve(address(_RAMVault), uint(-1));
     }
 
     event FeeApproverChanged(address indexed newAddress, address indexed oldAddress);
@@ -63,92 +67,107 @@ contract RAMv1Router is OwnableUpgradeSafe, VRFConsumerBase {
         }
     }
 
-    /// Route 1 : Buy LP for ETH
-    /// Route 2 : Buy LP for RAM
-
-    // Function sell Ram for ETH
-
-    // Function wrap ETH
-
-    // Function get price of RAM after sell
-
-    // Function get amount of RAM with a ETH buy
-
-    // Function get RAM needed to pair per ETH
-
-
+    // Markets buys YGY with 100% of the ETH deposited, then calls _swapYGYForRAMAndAddLiquidity
     function addLiquidityETHOnly(address payable to, bool autoStake) public payable {
         require(to != address(0), "Invalid address");
-        hardRAM[msg.sender] = hardRAM[msg.sender].add(msg.value);
 
-        uint256 buyAmount = msg.value.div(2);
+        uint256 buyAmount = msg.value;
         require(buyAmount > 0, "Insufficient ETH amount");
 
         _WETH.deposit{value : msg.value}();
 
-        (uint256 reserveWeth, uint256 reserveRAM) = getPairReserves();
-        uint256 outRAM = UniswapV2Library.getAmountOut(buyAmount, reserveWeth, reserveRAM);
+        (uint256 reserveWeth, uint256 reserveYGY) = getYGYWETHPairReserves();
+        uint256 outYGY = UniswapV2Library.getAmountOut(buyAmount, reserveWeth, reserveYGY);
 
-        _WETH.transfer(_RAMWETHPair, buyAmount);
+        _WETH.transfer(_YGYWETHPair, buyAmount);
 
-        (address token0, address token1) = UniswapV2Library.sortTokens(address(_WETH), _RAMToken);
-        IUniswapV2Pair(_RAMWETHPair).swap(_RAMToken == token0 ? outRAM : 0, _RAMToken == token1 ? outRAM : 0, address(this), "");
+        (address token0, address token1) = UniswapV2Library.sortTokens(address(_WETH), _YGYToken);
+        IUniswapV2Pair(_YGYWETHPair).swap(_YGYToken == token0 ? outYGY : 0, _YGYToken == token1 ? outYGY : 0, address(this), "");
+
+        hardRAMInYGY[msg.sender] = hardRAMInYGY[msg.sender].add(buyAmount);
+
+        _swapYGYForRAMAndAddLiquidity(buyAmount.div(2), to, autoStake);
+    }
+
+    // addLiquidityYGYOnly transfers approved YGY tokens to the contract and calls _swapYGYForRAMAndAddLiquidity
+    function addLiquidityYGYOnly(address payable to, uint256 amount, bool autoStake) public payable {
+        require(to != address(0), "Invalid address");
+        require(amount.div(2) > 2, "Insufficient token amount");
+        require(IERC20(_YGYToken).transferFrom(to, address(this), amount), "Approve tokens first");
+
+        hardRAMInYGY[msg.sender] = hardRAMInYGY[msg.sender].add(amount);
+
+        _swapYGYForRAMAndAddLiquidity(amount.div(2), to, autoStake);
+    }
+
+    // With buyAmount*2 amount of YGY tokens on the contract, this function market buys RAM with buyAmount
+    // of YGY and then calls _addLiquidity
+    function _swapYGYForRAMAndAddLiquidity(uint256 buyAmount, address payable to, bool autoStake) internal {
+        (uint256 reserveYGY, uint256 reserveRAM) = getYGYRAMPairReserves();
+        uint256 outRAM = UniswapV2Library.getAmountOut(buyAmount, reserveYGY, reserveRAM);
+
+        (address token0, address token1) = UniswapV2Library.sortTokens(_YGYToken, _RAMToken);
+        IUniswapV2Pair(_YGYRAMPair).swap(_RAMToken == token0 ? outRAM : 0, _RAMToken == token1 ? outRAM : 0, address(this), "");
 
         _addLiquidity(outRAM, buyAmount, to, autoStake);
 
-        // TODO: test sync method
         sync();
     }
 
-    function _addLiquidity(uint256 RAMAmount, uint256 wethAmount, address payable to, bool autoStake) internal {
-        (uint256 wethReserve, uint256 RAMReserve) = getPairReserves();
+   // _addLiquidity sends RAM, YGY tokens to the _YGYRAMPair contract and mints _YGYRAMPair LP tokens.
+   // It either auto stakes the LP tokens to the pool or sends them to the sender's address
+   function _addLiquidity(uint256 RAMAmount, uint256 YGYAmount, address payable to, bool autoStake) internal {
+        (uint256 YGYReserve, uint256 RAMReserve) = getYGYRAMPairReserves();
 
-        uint256 optimalRAMAmount = UniswapV2Library.quote(wethAmount, wethReserve, RAMReserve);
+        uint256 optimalRAMAmount = UniswapV2Library.quote(YGYAmount, YGYReserve, RAMReserve);
 
-        uint256 optimalWETHAmount;
+        uint256 optimalYGYAmount;
         if (optimalRAMAmount > RAMAmount) {
-            optimalWETHAmount = UniswapV2Library.quote(RAMAmount, RAMReserve, wethReserve);
+            optimalYGYAmount = UniswapV2Library.quote(RAMAmount, RAMReserve, YGYReserve);
             optimalRAMAmount = RAMAmount;
         }
         else
-            optimalWETHAmount = wethAmount;
+            optimalYGYAmount = YGYAmount;
 
-        assert(_WETH.transfer(_RAMWETHPair, optimalWETHAmount));
-        assert(IERC20(_RAMToken).transfer(_RAMWETHPair, optimalRAMAmount));
+        assert(IERC20(_YGYToken).transfer(_YGYRAMPair, optimalYGYAmount));
+        assert(IERC20(_RAMToken).transfer(_YGYRAMPair, optimalRAMAmount));
 
         if (autoStake) {
-            IUniswapV2Pair(_RAMWETHPair).mint(address(this));
-            _RAMVault.depositFor(to, 0, IUniswapV2Pair(_RAMWETHPair).balanceOf(address(this)));
+            IUniswapV2Pair(_YGYRAMPair).mint(address(this));
+            _RAMVault.depositFor(to, 0, IUniswapV2Pair(_YGYRAMPair).balanceOf(address(this)));
         }
         else
-            IUniswapV2Pair(_RAMWETHPair).mint(to);
+            IUniswapV2Pair(_YGYRAMPair).mint(to);
 
         //refund dust
         if (RAMAmount > optimalRAMAmount)
             IERC20(_RAMToken).transfer(to, RAMAmount.sub(optimalRAMAmount));
 
-        if (wethAmount > optimalWETHAmount) {
-            uint256 withdrawAmount = wethAmount.sub(optimalWETHAmount);
-            _WETH.withdraw(withdrawAmount);
-            to.transfer(withdrawAmount);
-        }
+        if (YGYAmount > optimalYGYAmount)
+            IERC20(_YGYToken).transfer(to, YGYAmount.sub(optimalYGYAmount));
     }
 
-    function getLPTokenPerEthUnit(uint ethAmt) public view  returns (uint liquidity){
-        (uint256 reserveWeth, uint256 reserveRAM) = getPairReserves();
-        uint256 outRAM = UniswapV2Library.getAmountOut(ethAmt.div(2), reserveWeth, reserveRAM);
-        uint _totalSupply =  IUniswapV2Pair(_RAMWETHPair).totalSupply();
+    function getLPTokenPerYGYUnit(uint ygyAmt) public view  returns (uint liquidity){
+        (uint256 reserveYGY, uint256 reserveRAM) = getYGYRAMPairReserves();
+        uint256 outRAM = UniswapV2Library.getAmountOut(ygyAmt.div(2), reserveYGY, reserveRAM);
+        uint _totalSupply =  IUniswapV2Pair(_YGYRAMPair).totalSupply();
 
-        (address token0, ) = UniswapV2Library.sortTokens(address(_WETH), _RAMToken);
-        (uint256 amount0, uint256 amount1) = token0 == _RAMToken ? (outRAM, ethAmt.div(2)) : (ethAmt.div(2), outRAM);
-        (uint256 _reserve0, uint256 _reserve1) = token0 == _RAMToken ? (reserveRAM, reserveWeth) : (reserveWeth, reserveRAM);
+        (address token0, ) = UniswapV2Library.sortTokens(_YGYToken, _RAMToken);
+        (uint256 amount0, uint256 amount1) = token0 == _RAMToken ? (outRAM, ygyAmt.div(2)) : (ygyAmt.div(2), outRAM);
+        (uint256 _reserve0, uint256 _reserve1) = token0 == _RAMToken ? (reserveRAM, reserveYGY) : (reserveYGY, reserveRAM);
         liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
     }
 
-    function getPairReserves() internal view returns (uint256 wethReserves, uint256 RAMReserves) {
-        (address token0,) = UniswapV2Library.sortTokens(address(_WETH), _RAMToken);
-        (uint256 reserve0, uint reserve1,) = IUniswapV2Pair(_RAMWETHPair).getReserves();
-        (wethReserves, RAMReserves) = token0 == _RAMToken ? (reserve1, reserve0) : (reserve0, reserve1);
+    function getYGYWETHPairReserves() internal view returns (uint256 wethReserves, uint256 YGYReserves) {
+        (address token0,) = UniswapV2Library.sortTokens(address(_WETH), _YGYToken);
+        (uint256 reserve0, uint reserve1,) = IUniswapV2Pair(_YGYWETHPair).getReserves();
+        (wethReserves, YGYReserves) = token0 == _YGYToken ? (reserve1, reserve0) : (reserve0, reserve1);
+    }
+
+    function getYGYRAMPairReserves() internal view returns (uint256 YGYReserves, uint256 RAMReserves) {
+        (address token0,) = UniswapV2Library.sortTokens(_YGYToken, _RAMToken);
+        (uint256 reserve0, uint reserve1,) = IUniswapV2Pair(_YGYRAMPair).getReserves();
+        (YGYReserves, RAMReserves) = token0 == _RAMToken ? (reserve1, reserve0) : (reserve0, reserve1);
     }
 
     // Function sync fee approver
@@ -194,6 +213,6 @@ contract RAMv1Router is OwnableUpgradeSafe, VRFConsumerBase {
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
         uint scaled = randomness / SCALIFIER;
         uint adjusted = scaled + OFFSET;
-        randomResult = randomness;
+        randomResult = adjusted;
     }
 }
